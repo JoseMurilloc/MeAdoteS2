da36e213fe19e334e82bff767945bfb743fbefc9
Jose Murillo <jooseemurillo@gmail.com>
2021-06-20 14:42:19 -0300

    fix: typescript types .env convert

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -2,7 +2,7 @@ export const configDatabase = {
 	user: process.env.DATABASE_USER,
 	password: process.env.DATABASE_PASSWORD,
 	host: process.env.DATABASE_HOST,
-	port: process.env.DATABASE_PORT,
+	port: Number(process.env.DATABASE_PORT),
 	database: process.env.DATABASE_NAME
 }
 
