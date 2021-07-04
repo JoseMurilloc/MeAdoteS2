19975cd1957263c784ccc61247270d46ecd7c822
Jose Murillo <jooseemurillo@gmail.com>
2021-07-04 13:02:14 -0300

    middleware validation credentials create session

--- server/src/app/routes/sessions.routes.ts
+++ server/src/app/routes/sessions.routes.ts
@@ -1,24 +1,15 @@
 import { Router } from 'express';
+import { Request, Response } from "express";
 import { AuthenticationUserService } from '../services/user/AuthenticationUserService'
-import { body, validationResult, check } from 'express-validator';
+import credentialsCheckBySession from '../middlewares/credentialsCheckBySession'
 
 const sessionsRoutes = Router();
 const authenticationUser = new AuthenticationUserService()
 
-sessionsRoutes.post(
-  '/',
-  body('email').isEmail().notEmpty(),
-  body('password').notEmpty().isLength({ min: 6 }),
-  check('password', 'Password is not permit caracteres special')
-    .matches(/^[\w&.\-]+$/, 'i'),
-  async (request, response) => {
+sessionsRoutes.post('/',
+  credentialsCheckBySession,
+  async (request: Request, response: Response) => {
     const { email, password } = request.body;
-
-    const errors = validationResult(request);
-    if (!errors.isEmpty()) {
-      return response.status(400).json({ errors: errors.array() });
-    }
-
     const { user, token } =
       await authenticationUser.execute({user: { email, password}})
 
